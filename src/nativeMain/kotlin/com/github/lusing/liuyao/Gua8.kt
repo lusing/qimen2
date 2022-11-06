1f231df90874b8e810a4959eae1a9e8e5f470484
Louis Liu <lusinga@126.com>
2022-11-07 02:21:49 +0800

    六十四卦

--- src/nativeMain/kotlin/com/github/lusing/liuyao/Gua8.kt
+++ src/nativeMain/kotlin/com/github/lusing/liuyao/Gua8.kt
@@ -1,15 +1,6 @@
 package com.github.lusing.liuyao
 
-enum class Gua(val value: Int) {
-    QIAN(0b000),
-    DUI  (0b001),
-    LI   (0b010),
-    ZHENG (0b011),
-    XUN  (0b100),
-    KAN  (0b101),
-    GEN  (0b110),
-    KUN  (0b111)
-}
+
 
 class Gua8 {
     val value: Int
